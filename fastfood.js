784593423757ea36cd0d83a35a93dd88dd9f87d3
abdelilleh <143023531+Moncif2005@users.noreply.github.com>
2023-09-17 03:31:33 +0100

    Add files via upload

---
+++ fastfood.js
@@ -0,0 +1,6 @@
+const humburugr = document.querySelector(".humburugr");
+humburugr.addEventListener("click",()=>{
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("active");
+
+});
